8db61a592c92f3102ff4d10418cc6f85619fd78a
WoranonMeethong <woranon17340@gmail.com>
2026-03-25 11:34:02 +0700

    p00

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello NestJS';
+  private app: Data[] = [
+    {service: "purchase-api"},
+    {version: "1.0.0"}
+  ]
+  getAppData(): Data[] {
+    return this.app;
   }
 }
